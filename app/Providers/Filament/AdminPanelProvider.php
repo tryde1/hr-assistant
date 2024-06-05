f825d39609c85ba17efa97a31092e97b02624a90
TooMuchRuss1a <demyanov.vv@dvfu.ru>
2024-06-05 20:09:39 +1000

    feat: reset password

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -46,6 +46,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->passwordReset();
     }
 }
